e97409997dcc879e870544cdc3fd3b4ed465e26e
Michael Aklilu <michaelaklilu@gmail.com>
2024-05-01 11:29:30 +0300

    Add a subtract function for unlimited parameters

--- javascript.js
+++ javascript.js
@@ -5,3 +5,11 @@ function add(...args){
     }
        return sum;
 }
+function subtract(...args){
+    let diff = args[0];
+    for(let i = 1;i<length.args;i++){
+       diff -= args[i];
+    }
+       return diff;
+}
+
